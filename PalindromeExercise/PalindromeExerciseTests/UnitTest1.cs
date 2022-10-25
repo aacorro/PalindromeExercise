4b665abb93ffd2c1f9dd82761e935fc038549f4b
Abdiel Corro <acorrop@outlook.com>
2022-10-25 18:49:45 -0500

    exercise completed

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,26 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("house", false)]
+        [InlineData("nun", true)]
+        [InlineData("Nun", true)]
+        [InlineData("madam", true)]
+        [InlineData("project", false)]
+        [InlineData("level", true)]
+        [InlineData("", false)]
+        
+        public void PalindromeTester(string word, bool expected)
         {
+            //Arrange
+            var input = new WordSmith();
 
+            //Act
+            bool actual = input.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
